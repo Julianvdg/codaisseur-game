cfc1d7513902348a685724c8a632d5eeb1796baa
Julian van der Giesen <julianvandergiesen@hotmail.com>
2016-09-09 16:24:40 +0200

    Entrance image

--- src/components/stages/Entrance.js
+++ src/components/stages/Entrance.js
@@ -1,13 +1,25 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
+import DialogBox from '../../components/DialogBox'
 
 class Entrance extends Component {
 
   render() {
 
+
+    let backgroundStyle = {
+      backgroundImage: 'url("http://res.cloudinary.com/juvdg/image/upload/v1473430931/entrance_jhdm3t.jpg")',
+      width: '880px',
+      height: '580px',
+      margin: '0 auto',
+      position: 'relative',
+      borderRadius: '10px',
+      border: '10px solid #d3d3d3',
+    };
+
     return(
-      <div>
-        <h1> Entrance Component </h1>
+      <div style={backgroundStyle}>
+        <DialogBox/>
       </div>
     )
   }
