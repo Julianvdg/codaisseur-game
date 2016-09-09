233e68f7a613764f87cb7c0bfa346edac9fbfed5
Julian van der Giesen <julianvandergiesen@hotmail.com>
2016-09-09 11:57:48 +0200

    Add bar background

--- src/components/stages/Bar.js
+++ src/components/stages/Bar.js
@@ -7,8 +7,14 @@ class Bar extends Component {
 
   render() {
 
+    let backgroundStyle = {
+      backgroundImage: 'url("http://res.cloudinary.com/juvdg/image/upload/v1473412718/bar_mkrl4k.jpg")',
+      width: '1200px',
+      height: '800px',
+    };
+
     return(
-      <div>
+      <div style={backgroundStyle}>
         <h1> Bar Component </h1>
       </div>
     )
