6fcdbbb8424b5f4df39c9e9f5664b255dd2db261
reinoptland <reinoptland@gmail.com>
2016-09-10 15:47:40 +0200

    start stage at the beginning

--- src/reducers/dialogBox.js
+++ src/reducers/dialogBox.js
@@ -1,7 +1,7 @@
 import { MESSAGE_DIALOG_BOX } from '../actions/message-dialog-box'
 import { EMPTY_DIALOG_BOX } from '../actions/empty-dialog-box'
 
-export default function updateDialogBox( state = "I'm a message!", { type, payload  } ){
+export default function updateDialogBox( state = "", { type, payload  } ){
   switch (type) {
     case MESSAGE_DIALOG_BOX :
       return payload
